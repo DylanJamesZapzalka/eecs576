f87df656b4ebc4cfe5079494217a489f96bc0cf8
Dylan Zapzalka <dylanjameszapzalka@hotmail.com>
2024-11-17 21:26:57 -0500

    Removed duplicate code

--- models.py
+++ models.py
@@ -16,22 +16,6 @@ class GCN(torch.nn.Module):
 
         return x
 
-
-class GAT(torch.nn.Module):
-    def __init__(self):
-        super(GAT, self).__init__()
-        self.conv1 = GATConv(768, 768)
-        self.conv2 = GATConv(768, 768)
-
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-
-        x = self.conv1(x, edge_index)
-        x = torch.nn.functional.relu(x)
-        x = self.conv2(x, edge_index)
-
-        return x
-
 class GAT(torch.nn.Module):
     def __init__(self):
         super(GAT, self).__init__()
