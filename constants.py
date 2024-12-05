65c6f468b391e3aaa3c88a5b0b0d4b6cd528204b
Alpha Romeo <hello@andychung.io>
2024-12-04 23:51:41 -0500

    adding aqa kg training pipeline

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ NQ_TEST_URL = 'https://dl.fbaipublicfiles.com/dpr/data/retriever/nq-test.qa.csv'
 # DO NOT CHANGE
 
 # SHOULD CHANGE
-DATASETS_DIR = '/home/dylanz/eecs576_datasets'
+DATASETS_DIR = '/mnt/c/Users/Andy/Github/rebel/AQA'
 # SHOULD CHANGE
 
 # DO NOT CHANGE
@@ -15,3 +15,9 @@ TRIVIA_TEST_FILE_NAME = os.path.join(DATASETS_DIR, 'trivia-test.qa.csv')
 NQ_TEST_FILE_NAME = os.path.join(DATASETS_DIR, 'nq-test.qa.csv')
 AMR_NQ_TRAIN_FILE_NAME = os.path.join(DATASETS_DIR, 'train.jsonl')
 AMR_NQ_TEST_FILE_NAME = os.path.join(DATASETS_DIR, 'test.jsonl')
+
+#
+AQA_TRAIN_FILE_NAME = os.path.join(DATASETS_DIR, 'retrieval_results_qa_train.json')
+AQA_TEST_FILE_NAME = os.path.join(DATASETS_DIR, 'retrieval_results_qa_test_wo_ans.json')
+AQA_VAL_FILE_NAME = os.path.join(DATASETS_DIR, 'retrieval_results_qa_valid_wo_ans.json')
+AQA_VAL_ANS = os.path.join(DATASETS_DIR, "qa_valid_flag.txt")
--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
-from fb_dpr_utils import has_answer
-from tqdm import tqdm
-from torch_geometric.data import Data
-import torch_geometric
-import torch
+import json
+import os
+import pickle
+from collections import defaultdict
+
 import numpy as np
+import torch
+from torch_geometric.data import Data
+from tqdm import tqdm
+from transformers import pipeline
+
+import constants
+from fb_dpr_utils import has_answer
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-def get_exact_match_score(question_embeddings, answers_array, dataset, k):
 
+def get_exact_match_score(question_embeddings, answers_array, dataset, k):
     exact_matches = 0
     for i in tqdm(range(len(question_embeddings)), desc='Evaluating over each question/answer'):
         # Get question and answers
@@ -18,21 +25,20 @@ def get_exact_match_score(question_embeddings, answers_array, dataset, k):
         # Get k nearest examples via DPR
         scores, retrieved_examples = dataset.get_nearest_examples('embeddings', question_embedding, k=k)
         retrieved_examples = retrieved_examples['text']
-        
+
         # Check each of the nearest passages for an exact match
         for retrieved_example in retrieved_examples:
-            match = has_answer(answers,retrieved_example)
+            match = has_answer(answers, retrieved_example)
             if match:
-                exact_matches +=1
+                exact_matches += 1
                 break
-    
+
     # Return the score
     score = exact_matches / len(question_embeddings)
     return score
 
 
 def get_data_kg_dpr(retrieved_examples, answers, nlp, link_type, number_of_links):
-
     # Get node feature vectors
     x = torch.tensor(retrieved_examples['embeddings']).to(device)
 
@@ -52,14 +58,13 @@ def get_data_kg_dpr(retrieved_examples, answers, nlp, link_type, number_of_links
 
 
 def get_data_amr(retrieved_examples, answers, ctx_encoder, ctx_tokenizer, amr_number_of_links):
-
     # Get passage embeddings and node features of amr graphs
     passage_embeddings = []
     nodes_list = []
     for passage in retrieved_examples:
-
         text = passage['text']
-        passage_tokens = ctx_tokenizer(text ,max_length=512,truncation=True,padding='max_length',return_tensors='pt').to(device)
+        passage_tokens = ctx_tokenizer(text, max_length=512, truncation=True, padding='max_length',
+                                       return_tensors='pt').to(device)
         passage_embedding = ctx_encoder(**passage_tokens)
         passage_embedding = passage_embedding.pooler_output
         passage_embedding = passage_embedding.cpu().detach().numpy()
@@ -67,9 +72,10 @@ def get_data_amr(retrieved_examples, answers, ctx_encoder, ctx_tokenizer, amr_nu
 
         # Get nodes and filter
         nodes = passage['nodes']
-        filtered_nodes = [node for node in nodes if len(node) > 3 and node != 'amr-unknown' and node != 'this' and node != 'person' and node != 'person' and node != 'name' and node != 'also' and node != 'multi-sentence']
+        filtered_nodes = [node for node in nodes if
+                          len(node) > 3 and node != 'amr-unknown' and node != 'this' and node != 'person' and node != 'person' and node != 'name' and node != 'also' and node != 'multi-sentence']
         nodes_list.append(filtered_nodes)
-    
+
     passage_embeddings = np.array(passage_embeddings)
 
     # Get node feature vectors
@@ -85,35 +91,25 @@ def get_data_amr(retrieved_examples, answers, ctx_encoder, ctx_tokenizer, amr_nu
     return data
 
 
-def get_data_kg(retrieved_examples, answers, nlp, number_of_links, link_type, ctx_encoder, ctx_tokenizer):
-
+def get_data_kg(retrieved_examples, answers, embeddings_dict, subgraphs):
     # Get passage embeddings and node features of amr graphs
     passage_embeddings = []
     passage_texts = []
     for passage in retrieved_examples:
-
         text = passage['text']
+        pid = passage['pid']
         passage_texts.append(text)
-        passage_tokens = ctx_tokenizer(text ,max_length=512,truncation=True,padding='max_length',return_tensors='pt').to(device)
-        passage_embedding = ctx_encoder(**passage_tokens)
-        passage_embedding = passage_embedding.pooler_output
-        passage_embedding = passage_embedding.cpu().detach().numpy()
+        passage_embedding = embeddings_dict[pid]
         passage_embeddings.append(passage_embedding)
-    
+
     passage_embeddings = np.array(passage_embeddings)
 
     # Get node feature vectors
-    x = torch.tensor(passage_embeddings)
+    x = torch.tensor(passage_embeddings, dtype=torch.float32)
     x = torch.squeeze(x)
 
     # Get the edge index
-    edge_index = None
-    if link_type == 'ssr':
-        edge_index = get_edge_index_shared_spacy_relationships(passage_texts, nlp, number_of_links)
-    elif link_type == 'se':
-        edge_index = get_edge_index_shared_entities(passage_texts, nlp, number_of_links)
-    else:
-        raise Exception('Invalid value for link_type.')
+    edge_index = get_edge_index_shared_entities_kg(retrieved_examples, subgraphs)
 
     # Get the labels and create the Data object
     y = get_labels(retrieved_examples, answers)
@@ -121,17 +117,17 @@ def get_data_kg(retrieved_examples, answers, nlp, number_of_links, link_type, ct
     return data
 
 
-def get_data_amg_plus_kg(retrieved_examples, answers, nlp, kg_number_of_links, kg_link_type, amr_number_of_links, ctx_encoder, ctx_tokenizer):
-
+def get_data_amg_plus_kg(retrieved_examples, answers, nlp, kg_number_of_links, kg_link_type, amr_number_of_links,
+                         ctx_encoder, ctx_tokenizer):
     # Get passage embeddings and node features of amr graphs
     passage_embeddings = []
     nodes_list = []
     passage_texts = []
     for passage in retrieved_examples:
-
         text = passage['text']
         passage_texts.append(text)
-        passage_tokens = ctx_tokenizer(text ,max_length=512,truncation=True,padding='max_length',return_tensors='pt').to(device)
+        passage_tokens = ctx_tokenizer(text, max_length=512, truncation=True, padding='max_length',
+                                       return_tensors='pt').to(device)
         passage_embedding = ctx_encoder(**passage_tokens)
         passage_embedding = passage_embedding.pooler_output
         passage_embedding = passage_embedding.cpu().detach().numpy()
@@ -139,9 +135,10 @@ def get_data_amg_plus_kg(retrieved_examples, answers, nlp, kg_number_of_links, k
 
         # Get nodes and filter
         nodes = passage['nodes']
-        filtered_nodes = [node for node in nodes if len(node) > 3 and node != 'amr-unknown' and node != 'this' and node != 'person' and node != 'person' and node != 'name' and node != 'also' and node != 'multi-sentence']
+        filtered_nodes = [node for node in nodes if
+                          len(node) > 3 and node != 'amr-unknown' and node != 'this' and node != 'person' and node != 'person' and node != 'name' and node != 'also' and node != 'multi-sentence']
         nodes_list.append(filtered_nodes)
-    
+
     passage_embeddings = np.array(passage_embeddings)
 
     # Get node feature vectors
@@ -151,7 +148,8 @@ def get_data_amg_plus_kg(retrieved_examples, answers, nlp, kg_number_of_links, k
     # Get the edge index for kg
     edge_index_kg = None
     if kg_link_type == 'ssr':
-        edge_index_kg = get_edge_index_shared_spacy_relationships(passage_texts, nlp, kg_number_of_links, return_list=True)
+        edge_index_kg = get_edge_index_shared_spacy_relationships(passage_texts, nlp, kg_number_of_links,
+                                                                  return_list=True)
     elif kg_link_type == 'se':
         edge_index_kg = get_edge_index_shared_entities(passage_texts, nlp, kg_number_of_links, return_list=True)
     else:
@@ -185,7 +183,7 @@ def get_edge_index_amr(nodes_list, amr_number_of_links, return_list=False):
                 edge_index.append([j, i])
         edge_index.append([i, i])
 
-    edge_index = list(map(list, set(map(tuple, edge_index)))) # Remove duplicates
+    edge_index = list(map(list, set(map(tuple, edge_index))))  # Remove duplicates
     if return_list:
         return edge_index
     else:
@@ -193,7 +191,6 @@ def get_edge_index_amr(nodes_list, amr_number_of_links, return_list=False):
 
 
 def get_edge_index_shared_spacy_relationships(retrieved_examples, nlp, number_of_links, return_list=False):
-
     entities_array = []
     children_array = []
     for retrieved_example in retrieved_examples:
@@ -222,17 +219,37 @@ def get_edge_index_shared_spacy_relationships(retrieved_examples, nlp, number_of
                 edge_index.append([j, i])
         edge_index.append([i, i])
 
-
-    edge_index = list(map(list, set(map(tuple, edge_index)))) # Remove duplicates
+    edge_index = list(map(list, set(map(tuple, edge_index))))  # Remove duplicates
     if return_list:
         return edge_index
     else:
         return torch.tensor(edge_index, dtype=torch.long)
 
 
+def get_edge_index_shared_entities_kg(retrieved_examples, subgraphs, return_list=False):
+    seen_entities = defaultdict(set)
+    for i, d in enumerate(retrieved_examples):
+        pid = d['pid']
+        kg = subgraphs[pid]
+        for triplet in kg:
+            seen_entities[triplet['head'].lower()].add(i)
+            seen_entities[triplet['tail'].lower()].add(i)
+    edge_index = []
+    for entity, key_set in tqdm(seen_entities.items()):
+        key_list = list(key_set)
+        for i in range(len(key_list)):
+            key_i = key_list[i]
+            for j in range(i + 1, len(key_list)):
+                key_j = key_list[j]
+                edge_index.append((key_i, key_j))
+                edge_index.append((key_j, key_i))
+    if return_list:
+        return edge_index
+    else:
+        return torch.tensor(edge_index, dtype=torch.long)
+
 
 def get_edge_index_shared_entities(retrieved_examples, nlp, number_of_links, return_list=False):
-
     entities_array = []
     children_array = []
     for retrieved_example in retrieved_examples:
@@ -258,19 +275,16 @@ def get_edge_index_shared_entities(retrieved_examples, nlp, number_of_links, ret
                 edge_index.append([j, i])
         edge_index.append([i, i])
 
-    edge_index = list(map(list, set(map(tuple, edge_index)))) # Remove duplicates
+    edge_index = list(map(list, set(map(tuple, edge_index))))  # Remove duplicates
     if return_list:
         return edge_index
     else:
         return torch.tensor(edge_index, dtype=torch.long)
 
 
-
-
 def get_labels_dpr(retrieved_examples, answers):
-
     labels = torch.zeros(len(retrieved_examples), dtype=torch.float)
-  
+
     # Check each of the nearest passages for an exact match
     for i in range(len(retrieved_examples)):
         # Get question and answers
@@ -284,9 +298,8 @@ def get_labels_dpr(retrieved_examples, answers):
 
 
 def get_labels(retrieved_examples, answers):
-
     labels = torch.zeros(len(retrieved_examples), dtype=torch.float)
-  
+
     # Check each of the nearest passages for an exact match
     for i in range(len(retrieved_examples)):
         # Get question and answers
@@ -297,3 +310,13 @@ def get_labels(retrieved_examples, answers):
             continue
     labels = torch.unsqueeze(labels, dim=1)
     return labels
+
+def get_kg(pkl_path, retrieved_examples, answers, embeddings_dict, subgraphs):
+    if os.path.exists(pkl_path):
+        with open(pkl_path, 'wb') as file:
+            return pickle.load(file)
+    else:
+        data = get_data_kg(retrieved_examples, answers, embeddings_dict, subgraphs)
+        with open(pkl_path, 'wb') as file:
+            pickle.dump(data, file)
+        return data
