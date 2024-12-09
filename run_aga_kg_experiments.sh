9b57ea5e805fdcd218a26a23a81d31a1bee72d1b
Josue Xavier Torres-Fonseca <josuetf@gl-login1.arc-ts.umich.edu>
2024-12-09 16:29:27 -0500

    remove unnecessary files

--- run_aga_kg_experiments.sh
+++
@@ -1,3 +0,0 @@
-python aqa_kg_main_experiments.py  --train_num_samples 5000 --test_num_samples 1000 --gnn_type gcn
-python aqa_kg_main_experiments.py  --train_num_samples 5000 --test_num_samples 1000 --gnn_type gat
-python aqa_kg_main_experiments.py  --train_num_samples 5000 --test_num_samples 1000 --gnn_type sage
